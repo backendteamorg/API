f5d894b3527d3a5d0ecad1f230cc5218b96a5d5a
alevscyg <Smolentcev.Aleksandr@yandex.ru>
2023-05-29 14:02:34 +0500

    first commit

--- apps/countriesnames/src/countriesnames.service.ts
+++ apps/countriesnames/src/countriesnames.service.ts
@@ -64,7 +64,7 @@ export class CountriesnamesService {
           }
           )
       }
-      return ArrCountriesNames
+      return await this.countriesNamesRepository.bulkCreate(ArrCountriesNames)
       
     }
     else{
@@ -88,4 +88,3 @@ export class CountriesnamesService {
       }})
   }
 }
-
